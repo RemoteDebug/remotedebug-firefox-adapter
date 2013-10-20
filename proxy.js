ed96372f59e0b0b33ab8cb3c11d2eab05da46929
Kenneth Auchenberg <kenneth@auchenberg.dk>
2013-10-20 20:22:08 +0200

    4 spaces as tabs.

--- proxy.js
+++ proxy.js
@@ -14,46 +14,46 @@ var Info = require('./lib/info');
 
 
 const defaultOptions = {
-  client : {
-    port : 6000
-  },
-  server : {
-    port : 8080
-  }
+    client: {
+        port: 6000
+    },
+    server: {
+        port: 8080
+    }
 
 };
 
 function RemoteDebugProxy(options) {
 
-  this.options = options || defaultOptions;
+    this.options = options || defaultOptions;
 
-  this.server = new Server(this.options);
-  this.client = new Client(this.options);
+    this.server = new Server(this.options);
+    this.client = new Client(this.options);
 
-  // Areas
-  new Worker(this.server, this.client);
-  new Network(this.server, this.client);
-  new DOM(this.server, this.client);
-  new Page(this.server, this.client);
-  new Console(this.server, this.client);
-  new Info(this.server, this.client);
-  new CSS(this.server, this.client);
+    // Areas
+    new Worker(this.server, this.client);
+    new Network(this.server, this.client);
+    new DOM(this.server, this.client);
+    new Page(this.server, this.client);
+    new Console(this.server, this.client);
+    new Info(this.server, this.client);
+    new CSS(this.server, this.client);
 
 
 }
 
 RemoteDebugProxy.prototype = {
 
-  start: function() {
+    start: function() {
 
-    var whenClientConnected = this.client.connect();
-    var whenServerStarted = this.server.start();
+        var whenClientConnected = this.client.connect();
+        var whenServerStarted = this.server.start();
 
-    Q.allSettled([whenClientConnected, whenServerStarted]).then(function() {
-      console.log('RemoteDebug proxy is ready.');
-    });
+        Q.allSettled([whenClientConnected, whenServerStarted]).then(function() {
+            console.log('RemoteDebug proxy is ready.');
+        });
 
-  }
+    }
 
 };
 
