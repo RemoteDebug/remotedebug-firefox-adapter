7f7558f005c59dc4357eec1a1d3aba06fe90a389
Kenneth Auchenberg <kenneth@auchenberg.dk>
2013-10-26 23:38:47 +0200

    Extracted the dom node caching into its own module.

--- proxy.js
+++ proxy.js
@@ -7,6 +7,7 @@ var log = require('./lib/logger');
 var Worker = require('./lib/worker');
 var Network = require('./lib/network');
 var DOM = require('./lib/dom');
+var DOMNodeCache = require('./lib/dom.node.cache');
 var CSS = require('./lib/css');
 var Page = require('./lib/page');
 var Console = require('./lib/console');
@@ -30,14 +31,16 @@ function RemoteDebugProxy(options) {
     this.server = new Server(this.options);
     this.client = new Client(this.options);
 
+    var domNodeCache = new DOMNodeCache();
+
     // Areas
     new Worker(this.server, this.client);
     new Network(this.server, this.client);
-    new DOM(this.server, this.client);
+    new DOM(this.server, this.client, domNodeCache);
+    new CSS(this.server, this.client, domNodeCache);
     new Page(this.server, this.client);
     new Console(this.server, this.client);
     new Info(this.server, this.client);
-    new CSS(this.server, this.client);
 
 
 }
