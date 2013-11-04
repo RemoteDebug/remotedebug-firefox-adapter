8d004c60aa82266fc03425c46999db455bdea191
Kenneth Auchenberg <kenneth@auchenberg.dk>
2013-11-04 23:35:03 +0100

    Extracted CSSComputedStyleProperty into its own model.

---
+++ models/CSSComputedStyleProperty.js
@@ -0,0 +1,12 @@
+
+function CSSComputedStyleProperty(name, value) {
+    this.name = name; // Computed style property name.
+    this.value = value; //Computed style property value.
+
+}
+
+CSSComputedStyleProperty.prototype = {
+
+};
+
+module.exports = CSSComputedStyleProperty;
