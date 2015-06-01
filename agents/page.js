81e884b8652ee3e2abba906d20ccc563168c9571
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-06-01 16:05:55 +0300

    Initial work on Page screencasting, but disable it for now.

--- agents/page.js
+++ agents/page.js
@@ -7,9 +7,12 @@ function Page (server, client) {
   this.server.on('Page.enable', this.enable.bind(this))
   this.server.on('Page.navigate', this.navigate.bind(this))
   this.server.on('Page.reload', this.reload.bind(this))
-  this.server.on('Page.canScreencast', this.canScreencast.bind(this))
   this.server.on('Page.canEmulate', this.canEmulate.bind(this))
   this.server.on('Page.hasTouchInputs', this.hasTouchInputs.bind(this))
+  this.server.on('Page.canScreencast', this.canScreencast.bind(this))
+  // this.server.on('Page.startScreencast', this.startScreencast.bind(this))
+  // this.server.on('Page.stopScreencast', this.stopScreencast.bind(this))
+  this.screncastingInterval;
 }
 
 util.inherits(Page, Core)
@@ -23,7 +26,9 @@ Page.prototype.canEmulate = function (request) {
 }
 
 Page.prototype.canScreencast = function (request) {
-  request.reply(true)
+  request.reply({
+    result: false
+  })
 }
 
 Page.prototype.hasTouchInputs = function (request) {
@@ -51,6 +56,44 @@ Page.prototype.navigate = function (request) {
       })
     }, 2000)
   })
+}
+
+Page.prototype.startScreencast = function (req) {
+
+  var page = this.client.getPage(req.data.pageId)
+
+  var captureScreencastFrame = function () {
+    page.Device.screenshotToDataURL(function(err, screenData) {
+      // page.DOM.document(function (err, elmDocument) {
+
+        screenData = screenData.replace('data:image/png;base64,', '')
+
+        var params = {
+          data: screenData,
+          metadata: {
+            pageScaleFactor: 1,
+            offsetTop: 0,
+            deviceWidth: 1000,
+            deviceHeight: 1000,
+            scrollOffsetX: 0,
+            scrollOffsetY: 0
+          }
+        }
+
+        req.sendNotification('Page.screencastFrame', params)
+
+      // });
+    })
+  }
+
+  this.screncastingInterval = setInterval(captureScreencastFrame, 1000)
+
+}
+
+Page.prototype.stopScreencast = function (req) {
+  if (this.screncastingInterval) {
+    clearInterval(this.screncastingInterval)
+  }
 }
 
 module.exports = Page
